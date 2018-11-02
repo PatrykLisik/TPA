3824cba96231e52ce15a4831aef38ae6969c8673
Patryk Lisik <patrykkbkak@gmail.com>
2018-11-02 15:52:08 +0100

    gui copy

---
+++ GUI/Models/TreeViewItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Models
+{
+    public class TreeViewItem
+    {
+        public TreeViewItem()
+        {
+            Children = new ObservableCollection<TreeViewItem>() { null };
+            this.m_WasBuilt = false;
+        }
+        public string Name { get; set; }
+        public ObservableCollection<TreeViewItem> Children { get; set; }
+        public bool IsExpanded
+        {
+            get { return m_IsExpanded; }
+            set
+            {
+                m_IsExpanded = value;
+                if (m_WasBuilt)
+                    return;
+                Children.Clear();
+                BuildMyself();
+                m_WasBuilt = true;
+            }
+        }
+
+        private bool m_WasBuilt;
+        private bool m_IsExpanded;
+        private void BuildMyself()
+        {
+            Random random = new Random();
+            for (int i = 0; i < random.Next(7); i++)
+                this.Children.Add(new TreeViewItem() { Name = "sample" + i });
+        }
+
+    }
+}
