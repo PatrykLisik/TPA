e0ab5a74126561385b0a57b96fb9786f354a337f
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2018-11-07 23:29:51 +0100

    fix

--- UnitTest/ReflectionMetadata/MetaData/TypeMetadataTests.cs
+++ UnitTest/ReflectionMetadata/MetaData/TypeMetadataTests.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnitTest;
 using System.Reflection;
 
 namespace Logic.ReflectionMetadata.Tests
