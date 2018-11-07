1d656da3daa8cf7b6717b941e307633c47137c6a
PatrykLisik <patrykkbk@gmail.com>
2018-11-07 19:12:31 +0100

    tui set up

---
+++ TUI2/MainClass.cs
@@ -0,0 +1,28 @@
+﻿using Logic.ReflectionMetadata;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace TUI2
+{
+    public class MainClass
+    {
+        static string pathToDll = @"..\..\..\Logic\bin\Debug\Logic.dll";
+        static TuiViewItem rootItem;
+
+        static void Main(string[] args)
+        {
+            LoadRootItem();
+
+
+        }
+
+        private static void LoadRootItem()
+        {
+            Assembly assembly = Assembly.LoadFrom(pathToDll);
+            AssemblyMetadata assemblyMetadata = new AssemblyMetadata(assembly);
+            rootItem = new TuiViewItem { Name = assemblyMetadata.m_Name, rest = assemblyMetadata.GetInternals() };
+        }
+
+    }
+}
---
+++ TUI2/TreeViewItem.cs
@@ -0,0 +1,76 @@
+﻿using Logic.ReflectionMetadata;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TUI2
+{
+    public class TuiViewItem
+    {
+        internal IEnumerable<IInternalGeter> rest;
+        public TuiViewItem()
+        {
+            Children = new ObservableCollection<TuiViewItem>() { null };
+            m_WasBuilt = false;
+        }
+        public string Name { get; set; }
+        public ObservableCollection<TuiViewItem> Children { get; set; }
+        public bool IsExpanded
+        {
+            get { return m_IsExpanded; }
+            set
+            {
+                m_IsExpanded = value;
+                if (m_WasBuilt)
+                    return;
+                Children.Clear();
+                BuildMyself();
+                m_WasBuilt = true;
+            }
+        }
+
+        private bool m_WasBuilt;
+        private bool m_IsExpanded;
+        private void BuildMyself()
+        {
+            foreach (IInternalGeter namespace_conteriner in rest)
+            {
+                IEnumerable<IInternalGeter>  internal_obj = namespace_conteriner.GetInternals();
+                if (internal_obj is null)
+                {
+                    Children.Add(new TuiViewItem
+                    {
+                        Name = namespace_conteriner.ToString(),
+                    });
+                }
+                else
+                {
+                    Children.Add(new TuiViewItem
+                    {
+                        Name = namespace_conteriner.ToString(),
+                        rest = internal_obj
+                    });
+                }
+
+
+
+            }
+
+        }
+
+        public string Print()
+        {
+            string ret =Name +"\n" ;
+            if (m_WasBuilt)
+            {
+                int start = 0;
+                foreach (IInternalGeter @internal in rest)
+                {
+                    ret += start.ToString()+". " + @internal.ToString()+"\n";
+                }
+            }
+
+            return ret;
+        }
+
+    }
+}
