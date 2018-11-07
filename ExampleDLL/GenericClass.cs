5a72364a7d9e1edf883ec37c3e275edcd3960dde
PatrykLisik <patrykkbk@gmail.com>
2018-11-07 17:58:39 +0100

    example project re-add

---
+++ ExampleDLL/GenericClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example
+{
+
+    public class GenericClass<T1,T2>
+    {
+        public T1 genericType1;
+        private T2 genericType2;
+
+        public T2 GenericType2 { get => genericType2; set => genericType2 = value; }
+        public IEnumerable<TM1> NestedGenericMethod<TM1, TM2>(IEnumerable<TM1> data1, IDictionary<String, TM2> data2)
+        {
+            return data1;
+        }
+    }
+}
