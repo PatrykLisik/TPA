673bcf3b016a290736e9a32b8d6f4d32a8eab436
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2018-11-26 21:45:53 +0100

    XML

---
+++ Logic/Serialization/IRepositoryActions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Serialization
+{
+    interface IRepositoryActions<T>
+    {
+        void SaveToRepository(T data, string fileName);
+        T LoadFromRepository(string fileName);
+    }
+}
---
+++ Logic/Serialization/XMLSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Logic.Serialization
+{
+    [Export(typeof(IRepositoryActions<>))]
+    public class XMLSerializer<T> : IRepositoryActions<T>
+    {
+        public T LoadFromRepository(string fileName)
+        {
+            DataContractSerializer s = new DataContractSerializer(typeof(T));
+            FileStream f = new FileStream(fileName, FileMode.Open);
+            XmlDictionaryReader r = XmlDictionaryReader.CreateTextReader(f, new XmlDictionaryReaderQuotas { MaxDepth = int.MaxValue });
+            T value = (T)s.ReadObject(r);
+            r.Close();
+            f.Close();
+            return value;
+        }
+
+        public void SaveToRepository(T data, string fileName)
+        {
+            FileStream f = new FileStream(fileName, FileMode.Create);
+            XmlWriterSettings settings = new XmlWriterSettings { Indent = true };
+            XmlWriter w = XmlWriter.Create(f, settings);
+            DataContractSerializer s = new DataContractSerializer(typeof(T));
+            s.WriteObject(w, data);
+            w.Close();
+            f.Close();
+        }
+    }
+}
