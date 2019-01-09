d9eb8249a2f8906528bde120b4af2a7e9a211ab9
PatrykLisik <patrykkbk@gmail.com>
2019-01-09 23:10:32 +0100

    debug string removed

--- ViewModel/TreeViewItems/TypeMetadataTreeViewItem.cs
+++ ViewModel/TreeViewItems/TypeMetadataTreeViewItem.cs
@@ -10,7 +10,7 @@ namespace ViewModel.TreeViewItems
         public TypeMetadataTreeViewItem(TypeMetadata typeMetadata)
         {
             _typeMetadata = typeMetadata;
-            Name = GenerateName() + "TypeMetadata";
+            Name = GenerateName();
 
 
         }
